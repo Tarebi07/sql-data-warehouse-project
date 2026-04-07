5a6f156785c829996c4df501998e1cd87aabaa44
Tarebi07 <Larrytarebi@gmail.com>
2026-04-07 16:38:00 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -21,6 +21,7 @@ GO
 USE Datawarehouse;
 GO
 
+--	Drop and recreate the'Datawarehouse database
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
 BEGIN
 	ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -33,7 +34,7 @@ USE Datawarehouse;
 GO
 
 
-
+--create  schema
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA gold;
